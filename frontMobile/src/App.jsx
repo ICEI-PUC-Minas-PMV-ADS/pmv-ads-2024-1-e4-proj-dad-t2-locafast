69536a6f737435c42106fcb1ae3165e1f8e142ef
Amanda Brito <amanda.brito.1427453@sga.pucminas.br>
2024-05-30 21:38:40 -0300

    Telas contrato, cadastroContrato, criação de dados mockados e modal

--- frontMobile/src/App.jsx
+++ frontMobile/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Dimensions, SafeAreaView, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {Dimensions, SafeAreaView, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import Header from './components/header';
-import Reserva from './pages/reserva';
 import Tabbar from './components/tabbar';
-
 import Routes from './pages/routes';
+import Reserva from './pages/reserva';
 
 function App() {
-
   return (
     <SafeAreaView style={styles.appBody}>
       <NavigationContainer>
+        <Header />
         <Routes />
+        <Tabbar />
       </NavigationContainer>
     </SafeAreaView>
   );
@@ -21,10 +21,10 @@ function App() {
 
 const styles = StyleSheet.create({
   appBody: {
-    display: "flex",
+    display: 'flex',
     height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width
-  }
-})
+    width: Dimensions.get('window').width,
+  },
+});
 
 export default App;
--- frontMobile/src/pages/routes.js
+++ frontMobile/src/pages/routes.js
@@ -1,37 +1,50 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Reserva from "./reserva";
-import cadastroReserva from "./cadastroReserva";
-import DetalhesReserva from "./detalhesReserva";
+import Reserva from './reserva';
+import CadastroReserva from './cadastroReserva';
+import DetalhesReserva from './detalhesReserva';
+import Contrato from './contrato';
+import CadastroContrato from './cadastroContrato';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 function ReservaRoutes() {
-    return (
-        <Stack.Navigator initialRouteName="Reservas">
-            <Stack.Screen name="Reserva" component={Reserva} />
-            <Stack.Screen name="detalhesReserva" component={DetalhesReserva} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator initialRouteName="Reservas">
+      <Stack.Screen name="Reservas" component={Reserva} />
+      <Stack.Screen name="DetalhesReserva" component={DetalhesReserva} />
+    </Stack.Navigator>
+  );
+}
+
+function ContratoRoutes() {
+  return (
+    <Stack.Navigator initialRouteName="ListaContratos">
+      <Stack.Screen name="ListaContratos" component={Contrato} />
+      <Stack.Screen name="CadastroDeContrato" component={CadastroContrato} />
+    </Stack.Navigator>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 function Routes() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="Home"
-                component={ReservaRoutes}
-                options={{ headerShown: false }}
-            />
-            <Tab.Screen
-                name="Cadastro Reserva"
-                component={cadastroReserva}
-            />
-        </Tab.Navigator>
-    )
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={ReservaRoutes}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen name="CadastroReserva" component={CadastroReserva} />
+      <Tab.Screen
+        name="Contratos"
+        component={ContratoRoutes}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default Routes;
