777205316552c624b45793c786476b8b9cc03d77
DavidGomes1 <davig.bhte@gmail.com>
2024-06-02 21:40:04 -0300

    Mudança na parte de clientes.

--- frontMobile/src/pages/cliente.jsx
+++ frontMobile/src/pages/cliente.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, FlatList, Dimensions } from "react-native";
 
 import Card from "../components/card";
 import clientes from "../data/cliente"
 
 export default ({ navigation }) => {
+    const [data, setData] = useState(clientes);
 
-    const [data, setData] = useState(clientes)
+    useEffect(() => {
+        // Esta função é chamada toda vez que a tela é carregada ou quando a lista de clientes é atualizada
+        console.log("Data atualizada:", data);
+    }, [data]);
+
+    const updateList = (updatedCliente) => {
+        if (updatedCliente) {
+            // Atualiza o cliente na lista
+            const newData = data.map(item => {
+                if (item._id === updatedCliente._id) {
+                    return updatedCliente;
+                }
+                return item;
+            });
+            setData(newData);
+        } else {
+            // Remove o cliente da lista
+            const newData = data.filter(item => item._id !== updatedCliente?._id);
+            setData(newData);
+        }
+    };
 
     return (
         <View style={styles.body}>
@@ -17,17 +38,19 @@ export default ({ navigation }) => {
                     <Card
                         keyTitle={"_id"}
                         data={{ ...item }}
-                        onPress={() => navigation.navigate('detalhesCliente', { cliente: item })}
+                        onPress={() => navigation.navigate('DetalhesCliente', { cliente: item, updateList })}
                         keysToRender={[
                             "clienteId",
                             "nome",
                             "cpf",
                             "rg"
-                        ]} />}
+                        ]}
+                    />
+                }
             />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     body: {
@@ -37,4 +60,4 @@ const styles = StyleSheet.create({
         alignItems: "center",
         width: Dimensions.get('window').width
     }
-})
+});
--- frontMobile/src/pages/detalhesCliente.jsx
+++ frontMobile/src/pages/detalhesCliente.jsx
@@ -1,89 +1,94 @@
 import React from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, ScrollView } from "react-native";
 
 export default ({ route }) => {
     const { cliente } = route.params;
 
     return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                value={cliente.idCliente}
-                placeholder="ID Cliente"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.numeroCnh}
-                placeholder="Número da CNH"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.validadeCnh}
-                placeholder="Validade da CNH"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.estadoEmissor}
-                placeholder="Estado Emissor"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.nome}
-                placeholder="Nome"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.cpf}
-                placeholder="CPF"
-                editable={false}
-                keyboardType="numeric"
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.rg}
-                placeholder="RG"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.telefone}
-                placeholder="Telefone"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.email}
-                placeholder="Email"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.dataNascimento}
-                placeholder="Data de Nascimento"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.status}
-                placeholder="Status"
-                editable={false}
-            />
-            <TextInput
-                style={styles.input}
-                value={cliente.genero}
-                placeholder="Gênero"
-                editable={false}
-            />
-        </View>
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    value={cliente.idCliente}
+                    placeholder="ID Cliente"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.numeroCnh}
+                    placeholder="Número da CNH"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.validadeCnh}
+                    placeholder="Validade da CNH"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.estadoEmissor}
+                    placeholder="Estado Emissor"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.nome}
+                    placeholder="Nome"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.cpf}
+                    placeholder="CPF"
+                    editable={false}
+                    keyboardType="numeric"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.rg}
+                    placeholder="RG"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.telefone}
+                    placeholder="Telefone"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.email}
+                    placeholder="Email"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.dataNascimento}
+                    placeholder="Data de Nascimento"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.status}
+                    placeholder="Status"
+                    editable={false}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={cliente.genero}
+                    placeholder="Gênero"
+                    editable={false}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
+    scrollViewContainer: {
+        flexGrow: 1,
+    },
     container: {
         flex: 1,
         padding: 16,
