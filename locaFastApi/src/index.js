701978d01773ab18a2335cfbc42a9c1b0734bb6f
Amanda Brito <amanda.brito.1427453@sga.pucminas.br>
2024-06-21 20:58:21 -0300

    Permitir todas as origens e métodos no CORS

--- locaFastApi/src/index.js
+++ locaFastApi/src/index.js
@@ -8,30 +8,41 @@ const swaggerDocument = require('../swagger-config');
 const checkToken = require('./config/auth/checkToken');
 const createInitialData = require('./config/db/initialData');
 
+// Variáveis de ambiente
 require('dotenv').config();
 
 const app = express();
 
+// Permitir todas as origens e métodos
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
 createInitialData();
+
 app.use(express.json());
 
+// Rotas API
 const clienteRoutes = require('./routes/clienteRoutes');
 const carroRoutes = require('./routes/carroRoutes');
 const reservaRoutes = require('./routes/reservaRoutes');
 const colaboradorRoutes = require('./routes/colaboradorRoutes');
 const loginRoutes = require('./routes/loginRoutes');
+
 const contratoRoutes = require('./routes/contratoRoutes');
 
 app.use('/login', loginRoutes);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Rotas que não precisam de autenticação favor inserir acima do app.use(checkToken)
 app.use(checkToken);
 
 app.use('/cliente', clienteRoutes);
@@ -40,9 +51,13 @@ app.use('/reserva', reservaRoutes);
 app.use('/colaborador', colaboradorRoutes);
 app.use('/contrato', contratoRoutes);
 
-mongoose.connect(process.env.STRING_CONEXAO)
-    .then(() => {
-        console.log("MongoDB conectado!");
-        app.listen(3000);  // Certifique-se de que a aplicação escuta na porta 3000
-    })
-    .catch((err) => console.log(err));
+// Conexão com o banco
+mongoose.connect(
+    process.env.STRING_CONEXAO
+).then(() => {
+    console.log("MongoDB conectado!");
+    app.listen(3001);
+})
+.catch((err) => console.log(err));
+
+app.listen(3000);
