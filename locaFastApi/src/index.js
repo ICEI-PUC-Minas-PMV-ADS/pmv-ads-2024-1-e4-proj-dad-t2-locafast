e87e1037e9ac63eb88772f621474bd4789f42ae3
Amanda Brito <amanda.brito.1427453@sga.pucminas.br>
2024-06-22 00:21:46 -0300

    Remover dependência de process.env.PORT e garantir configuração correta de CORS

--- locaFastApi/src/index.js
+++ locaFastApi/src/index.js
@@ -11,22 +11,22 @@ require('dotenv').config();
 const app = express();
 
 // Configuração do CORS para permitir todas as origens
-const corsOptions = {
+app.use(cors({
   origin: '*', // Permitir todas as origens
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
-};
+}));
 
+// Adicionar logs para diagnosticar solicitações
 app.use((req, res, next) => {
+  console.log(`Recebendo solicitação: ${req.method} ${req.path}`);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
-
-app.use(cors(corsOptions));
 
 app.use(
   express.urlencoded({
@@ -63,7 +63,7 @@ mongoose.connect(
   process.env.STRING_CONEXAO
 ).then(() => {
   console.log("MongoDB conectado!");
-  const PORT = process.env.PORT || 3000; // Certifique-se de que a porta é 3000
+  const PORT = 3000; // Certifique-se de que a porta é 3000
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
