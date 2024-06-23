596f0786d6997d1f527446c1cc5e0162faca02ae
Amanda Brito <amanda.brito.1427453@sga.pucminas.br>
2024-06-22 21:11:51 -0300

    Ajustes finais na tabela de contratos, correção da barra lateral e ajuste de layout

--- frontWeb/src/components/tableContrato.jsx
+++ frontWeb/src/components/tableContrato.jsx
@@ -1,89 +1,78 @@
 import React from 'react';
-import './style/tableContrato.css'; // Atualize o caminho para o CSS corretamente
+import { useNavigate } from 'react-router-dom';
+import './style/tableContrato.css';
 
-const TableContrato = ({
-    title,
-    btnTxt,
-    goto,
-    trs,
-    data,
-    editingId,
-    editFormData,
-    handleEditFormChange,
-    handleEditClick,
-    handleCancelClick,
-    handleSaveClick,
-    onDelete
-}) => {
-    return (
-        <div className="table-container">
-            <section className="table-header">
-                <div className="table-title">
-                    <h1>{title}</h1>
-                </div>
-                <div className="button-container">
-                    <button onClick={() => window.location.href = goto}>{btnTxt}</button>
-                </div>
-            </section>
-            <section className="table-body">
-                <table className="styled-table">
-                    <thead>
-                        <tr>
-                            {trs.map((tr, index) => (
-                                <th key={index}>{tr}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((contrato) => (
-                            editingId === contrato.id ? (
-                                <tr key={contrato.id}>
-                                    {/* Renderização dos campos de edição */}
-                                    <td>{contrato.id}</td>
-                                    <td><input type="text" name="clienteId" value={editFormData.clienteId} onChange={handleEditFormChange} /></td>
-                                    <td><input type="text" name="reservaId" value={editFormData.reservaId} onChange={handleEditFormChange} /></td>
-                                    <td><input type="text" name="carroId" value={editFormData.carroId} onChange={handleEditFormChange} /></td>
-                                    <td><input type="text" name="colaboradorId" value={editFormData.colaboradorId} onChange={handleEditFormChange} /></td>
-                                    <td>
-                                        <select name="status" value={editFormData.status} onChange={handleEditFormChange}>
-                                            <option value="ativo">Ativo</option>
-                                            <option value="inativo">Inativo</option>
-                                            <option value="cancelado">Cancelado</option>
-                                        </select>
-                                    </td>
-                                    <td><input type="date" name="dataRetirada" value={editFormData.dataRetirada} onChange={handleEditFormChange} /></td>
-                                    <td><input type="date" name="dataDevolucao" value={editFormData.dataDevolucao} onChange={handleEditFormChange} /></td>
-                                    <td><input type="text" name="agenciaRetirada" value={editFormData.agenciaRetirada} onChange={handleEditFormChange} /></td>
-                                    <td><input type="text" name="agenciaDevolucao" value={editFormData.agenciaDevolucao} onChange={handleEditFormChange} /></td>
-                                    <td>
-                                        <button onClick={handleSaveClick}>Salvar</button>
-                                        <button onClick={handleCancelClick}>Cancelar</button>
-                                    </td>
-                                </tr>
-                            ) : (
-                                <tr key={contrato.id}>
-                                    <td>{contrato.id}</td>
-                                    <td>{contrato.clienteId}</td>
-                                    <td>{contrato.reservaId}</td>
-                                    <td>{contrato.carroId}</td>
-                                    <td>{contrato.colaboradorId}</td>
-                                    <td>{contrato.status}</td>
-                                    <td>{contrato.dataRetirada}</td>
-                                    <td>{contrato.dataDevolucao}</td>
-                                    <td>{contrato.agenciaRetirada}</td>
-                                    <td>{contrato.agenciaDevolucao}</td>
-                                    <td>
-                                        <button onClick={() => handleEditClick(contrato)}>Editar</button>
-                                        <button onClick={() => onDelete(contrato.id)}>Deletar</button>
-                                    </td>
-                                </tr>
-                            )
-                        ))}
-                    </tbody>
-                </table>
-            </section>
+const TableContrato = ({ title, btnTxt, goto, trs, data, editingId, editFormData, handleEditFormChange, handleEditClick, handleCancelClick, handleSaveClick, onDelete }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <div className="table-title">
+          <h1>{title}</h1>
         </div>
-    );
+        <div className="button-container">
+          <button onClick={() => navigate(goto)}>{btnTxt}</button>
+        </div>
+      </div>
+      <div className="table-body">
+        <table className="styled-table">
+          <thead>
+            <tr>
+              {trs.map((tr, index) => (
+                <th key={index}>{tr}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((contrato) => (
+              editingId === contrato.id ? (
+                <tr key={contrato.id}>
+                  <td>{contrato.id}</td>
+                  <td><input type="text" name="clienteId" value={editFormData.clienteId} onChange={handleEditFormChange} /></td>
+                  <td><input type="text" name="reservaId" value={editFormData.reservaId} onChange={handleEditFormChange} /></td>
+                  <td><input type="text" name="carroId" value={editFormData.carroId} onChange={handleEditFormChange} /></td>
+                  <td><input type="text" name="colaboradorId" value={editFormData.colaboradorId} onChange={handleEditFormChange} /></td>
+                  <td>
+                    <select name="status" value={editFormData.status} onChange={handleEditFormChange}>
+                      <option value="ativo">Ativo</option>
+                      <option value="inativo">Inativo</option>
+                      <option value="cancelado">Cancelado</option>
+                    </select>
+                  </td>
+                  <td><input type="date" name="dataRetirada" value={editFormData.dataRetirada} onChange={handleEditFormChange} /></td>
+                  <td><input type="date" name="dataDevolucao" value={editFormData.dataDevolucao} onChange={handleEditFormChange} /></td>
+                  <td><input type="text" name="agenciaRetirada" value={editFormData.agenciaRetirada} onChange={handleEditFormChange} /></td>
+                  <td><input type="text" name="agenciaDevolucao" value={editFormData.agenciaDevolucao} onChange={handleEditFormChange} /></td>
+                  <td>
+                    <button onClick={handleSaveClick}>Salvar</button>
+                    <button onClick={handleCancelClick}>Cancelar</button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={contrato.id}>
+                  <td>{contrato.id}</td>
+                  <td>{contrato.clienteId}</td>
+                  <td>{contrato.reservaId}</td>
+                  <td>{contrato.carroId}</td>
+                  <td>{contrato.colaboradorId}</td>
+                  <td>{contrato.status}</td>
+                  <td>{contrato.dataRetirada}</td>
+                  <td>{contrato.dataDevolucao}</td>
+                  <td>{contrato.agenciaRetirada}</td>
+                  <td>{contrato.agenciaDevolucao}</td>
+                  <td>
+                    <button onClick={() => handleEditClick(contrato)}>Editar</button>
+                    <button onClick={() => onDelete(contrato.id)}>Deletar</button>
+                  </td>
+                </tr>
+              )
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default TableContrato;
--- frontWeb/src/main.jsx
+++ frontWeb/src/main.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Login from "./pages/login.jsx";
-import Cadastro from './pages/cadastro.jsx';
-import Carro from './pages/carro.jsx';
-import CadastroCarro from './pages/cadastroCarro.jsx';
-import Cliente from './pages/cliente.jsx';
-import Reserva from './pages/reserva.jsx';
-import CadastroCliente from './pages/cadastroCliente.jsx';
-import CadastroReserva from './pages/cadastroReserva.jsx';
 import Contrato from './pages/contrato.jsx';
 import CriarContrato from './pages/criarContrato.jsx';
-import ReservaStep1 from './components/reservaStep1.jsx';
-import ReservaStep2 from './components/reservaStep2.jsx';
-import ReservaStep3 from './components/reservaStep3.jsx';
-
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -31,59 +19,14 @@ const router = createBrowserRouter([
         element: <Navigate to="contrato" />
       },
       {
-        path: "reserva",
-        element: <Reserva />,
-      },
-      {
-        path: "cadastroreserva",
-        element: <CadastroReserva />,
-        children: [
-          {
-            path: '',
-            element: <Navigate to="reserva-passo-1" />
-          },
-          {
-            path: 'reserva-passo-1',
-            element: <ReservaStep1 />
-          },
-          {
-            path: 'reserva-passo-2',
-            element: <ReservaStep2 />
-          },
-          {
-            path: 'reserva-passo-3',
-            element: <ReservaStep3 />
-          }
-        ]
-      },
-      {
         path: "contrato",
         element: <Contrato />,
       },
       {
-        path: "carro",
-        element: <Carro />,
-      },
-      {
-        path: "cliente",
-        element: <Cliente />,
-      },
-      {
-        path: "cadastro",
-        element: <Cadastro />,
-      },
-      {
-        path: "cadastrocarro",
-        element: <CadastroCarro />
-      },
-      {
-        path: "cadastrocliente",
-        element: <CadastroCliente />
-      },
-      {
         path: "criarcontrato",
         element: <CriarContrato />
-      }
+      },
+      // Outras rotas
     ],
   }
 ]);
